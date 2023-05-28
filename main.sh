ed1f9fe55ce8b5428325cef3e170aa13058292d8
tugsatenes <tugsatenes@proton.me>
2023-05-28 13:25:17 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -18,5 +18,34 @@ timedatectl set-ntp true
 # Şu anlık sadece nvme desteği var
 # Diski gösterir
 
-#fdisk /dev/nvme0n1 -l
-fdisk /dev/sda -l
+fdisk /dev/nvme0n1 -l
+#fdisk /dev/sda -l
+
+# diski şu şekilde biçimlendirir:
+# 512MB UEFI disk bölümü (1)
+# 1GB Swap (takas) alanı (2)
+# Diskin kalanı ise Kök (/) bölümü (3)
+
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << FDISK_CMDS  | fdisk /dev/nvme0n1
+g      # Yeni GPT bölümü oluşturur
+n      # yeni bölüm ekler
+1      # Bölüm numarası
+       # Varsayılan olarak ilk sektör alanını boş bırakıyoruz 
++512MB # Bölüm boyutu
+n      # 
+2      # 
+       #  
++1GB   #  
+t      # Bölüm tipini değiştirir
+1      # 
+uefi   # EFI bölümü
+t      # 
+2      # 
+swap   # Takas alanı
+n      #
+3      #
+       #
+       # Geriye kalan bütün alan
+w      # bölümlendirme tablosunu yazar ve çıkar
+FDISK_CMDS
+
