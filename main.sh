452c2aef257846b219f82d8208434f6ef63d75f1
tugsatenes <tugsatenes@proton.me>
2023-05-28 12:54:37 +0300

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+# Klavyeyi Türkçe yapar
+loadkeys trq
+
+# Efiyi kontrol eder
+# Şu anlık sadece UEFI destekli, yardımlarınızı bekliyorum!
+
+ls /sys/firmware/efi/efivars
+
+
+# Şu anlık sadece kablolu dhcp kurulumlar destekli
+#İnterneti kontrol eder
+
+ping -c 5 https://archlinux.org/
+
+
+# Sistem saatini günceller
+
+timedatectl set-ntp true
+
+
+# Şu anlık sadece nvme desteği var
+# Diski gösterir
+
+#fdisk /dev/nvme0n1 -l
+fdisk /dev/sda -l
